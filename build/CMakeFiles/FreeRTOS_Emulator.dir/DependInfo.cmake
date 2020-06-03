0cec00be82780d725a8d2380aa3507948720518f
Raphael Mayr <raphael.mayr@tum.de>
2020-06-03 10:17:43 +0200

    initial push

---
+++ build/CMakeFiles/FreeRTOS_Emulator.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/AsyncIO/AsyncIO.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/AsyncIO/AsyncIO.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/croutine.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/croutine.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/list.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/list.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/portable/GCC/Posix/port.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/portable/GCC/Posix/port.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/portable/MemMang/heap_3.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/portable/MemMang/heap_3.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/queue.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/queue.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/tasks.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/tasks.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/FreeRTOS_Kernel/timers.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/FreeRTOS_Kernel/timers.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Ball.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Ball.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Draw.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Draw.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Event.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Event.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Font.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Font.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Sound.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Sound.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/lib/Gfx/TUM_Utils.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/lib/Gfx/TUM_Utils.c.o"
+  "/home/rtos-sim/ESPL-SS20_SpaceInv/src/main.c" "/home/rtos-sim/ESPL-SS20_SpaceInv/build/CMakeFiles/FreeRTOS_Emulator.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../lib/FreeRTOS_Kernel/include"
+  "../lib/FreeRTOS_Kernel/portable/GCC/Posix"
+  "../include"
+  "../lib/Gfx/include"
+  "../lib/AsyncIO/include"
+  "../lib/tracer/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
